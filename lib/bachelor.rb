b27ea5c16a82189a878bac221c614ab0fb3c034a
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-15 20:33:27 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,4 @@
-require "pry" 
-
 def get_first_name_of_season_winner(data, season)
-  # code here
   winner_name = ""
   data[season].each do |contestant|
     #binding.pry
@@ -13,7 +10,6 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
   name = ""
   data.each do |season, contestants_array|
     contestants_array.each do |contestant|
@@ -26,30 +22,35 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-   code here
   hometown_count = 0 
-  data[season].each do |contestant|
-    if contestant["hometown"] == hometown
-      hometown_count += 1
+  data.each do |season, contestants_array|
+    contestants_array.each do |contestant|
+      if contestant["hometown"] == hometown
+        hometown_count += 1
+      end
     end
   end
   hometown_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants_array|
+    contestants_array.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
   contestant_count = 0
   total_age = 0
   data[season].each do |contestant|
     contestant_count += 1
     total_age += contestant["age"].to_i
   end
-  (total_age / contestant_count.to_f).round(0)
-  #(total_age / contestant_count).round
+  (total_age / contestant_count.to_f).round
 end
 
 
